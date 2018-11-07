1fbba02aa3ceb7990f709d86ba50308eec474ff7
johannpino <johannpino@gmail.com>
2018-11-07 02:16:44 -0300

    Updates

---
+++ precache-manifest.c3efeecaaf9fbfc63d3304ab35ba7a6b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "b4581c3cf0ca89a7dceb",
+    "url": "/todo-app-react/static/js/runtime~main.b4581c3c.js"
+  },
+  {
+    "revision": "0b6432d1324feb55a5db",
+    "url": "/todo-app-react/static/js/main.0b6432d1.chunk.js"
+  },
+  {
+    "revision": "bad28fd0f0b5d21e6cad",
+    "url": "/todo-app-react/static/js/1.bad28fd0.chunk.js"
+  },
+  {
+    "revision": "0b6432d1324feb55a5db",
+    "url": "/todo-app-react/static/css/main.23ff8895.chunk.css"
+  },
+  {
+    "revision": "6bb4fd24e6e69c46b18ecc20c2d42a34",
+    "url": "/todo-app-react/index.html"
+  }
+];
